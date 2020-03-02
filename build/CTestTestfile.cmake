8a51c50bf59081d9dbb119ec8268b39f602af6fd
Yannik Eckert <yannik.eckert@gmx.de>
2020-03-02 17:02:14 +0000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src
+# Build directory: /home/ubuntu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(demos/dnn_rotate)
+subdirs(demos/docking)
+subdirs(demos/fiducial_follow)
+subdirs(demos/move_demo)
